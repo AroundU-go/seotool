269e72efef2717fcf425e2758ceaafd19877e8ca
uddipangoswami <uddipangoswami101@gmail.com>
2026-04-12 00:14:30 +0530

    Add Google Analytics gtag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,19 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-TWFT8MWFHJ"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-TWFT8MWFHJ');
+            `
+          }}
+        />
         <meta charSet="UTF-8" />
         <link rel="icon" type="image/jpeg" href="/favicon.jpg" />
         
