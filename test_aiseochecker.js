1bbd3cc6d96451c75113441e5997870c79f5f779
uddipangoswami <uddipangoswami101@gmail.com>
2026-04-20 11:55:46 +0530

    Update VebAPI key

--- test_aiseochecker.js
+++ test_aiseochecker.js
@@ -1,7 +1,7 @@
 const url = "https://vebapi.com/api/seo/aiseochecker?website=vebapi.com";
 fetch(url, {
   headers: {
-    "X-API-KEY": "f1aa74ef-16ff-4713-ae11-ab5cd3523246",
+    "X-API-KEY": "059f4e4e-2479-4f57-af3e-e64f69e8c4e7",
     "Content-Type": "application/json"
   }
 }).then(res => res.json()).then(data => console.log(JSON.stringify(data, null, 2))).catch(err => console.error(err));
